9485577480ab1e63537a497f345ddea91bd70241
WJcaine <billy.caine@gmail.com>
2019-11-12 17:02:19 +0000

    Does some stuff on insert_data function - which is slighlty not right apparently

--- database/seeding.py
+++ database/seeding.py
@@ -1,8 +1,11 @@
 import psycopg2
 from config import config
- 
+from data.testdata import outputs
 
-data = {
+print(outputs)
+
+
+user = {
      'first_name' : "Barry",
      'surname' : "Chuckle",
      'email' : "BigBoiBaz@gmail.com",
@@ -11,18 +14,8 @@ data = {
      'password' : "RIP"
  }
 
-data2 = [
-     "Barry",
-     "Chuckle",
-     "BigBoiBaz@gmail.com",
-     1,
-     "BazziChuck",
-     "RIP"
-
- ]
  
-def insert_data(data):
-    """ Insert new user into users table"""
+def insert_user(user):
     sql = """INSERT INTO users(first_name, surname, email, sensor_id,username,password)
              VALUES(%s, %s, %s, %s, %s, %s) RETURNING *;
              SELECT * FROM users;
@@ -30,14 +23,14 @@ def insert_data(data):
     conn = None
     user_id = None
     try:
-        # read database configuration
+        # read userbase configuration
         params = config()
-        # connect to the PostgreSQL database
+        # connect to the PostgreSQL userbase
         conn = psycopg2.connect(**params)
         # create a new cursor
         cur = conn.cursor()
         # execute the INSERT statement
-        cur.execute(sql, (data['first_name'],data['surname'],data['email'],data['sensor_id'],data['username'],data['password']))
+        cur.execute(sql, (user['first_name'],user['surname'],user['email'],user['sensor_id'],user['username'],user['password']))
         # get the generated id back
         users = cur.fetchmany(100)
         # commit the changes to the database
@@ -52,4 +45,30 @@ def insert_data(data):
  
     return users
 
-print(insert_data(data))
+def insert_data (data):
+    sql = """INSERT INTO data(reading_id,temperature,pressure,humidity,TVOC,gasBaseLine,user_id)
+             VALUES(%s, %s, %s, %s, %s, %s,%s) RETURNING *;
+             SELECT * FROM data;
+             """
+    conn = None
+    reading_id = None
+    try:
+        print(data['af4eb1']['temp_mean'])
+        params=config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute(sql,(data['af4eb1']['temp_mean'],data['af4eb1']['pressure_mean'],data['af4eb1']['humidity_mean'],data['af4eb1']['tvoc_mean'],data['af4eb1']['gas_baseline'],1))
+        data = cur.fetchmany(200)
+        conn.commit()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+ 
+    return data
+
+for i in outputs:
+    insert_data(i)
+
