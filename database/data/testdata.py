499101808afde13fd260e09d4eea083d1f7d1728
jonnowelch <jonnowelch@yahoo.com>
2019-11-12 14:03:20 +0000

    sorted import error and added first batch of test dummy data to a file

---
+++ database/data/testdata.py
@@ -0,0 +1,99 @@
+1573563468.4146097
+{'af4eb1': {'temp_mean': 29.5, 'pressure_mean': 989.0419999999999, 'humidity_mean': 32.7721, 'tvoc_mean': 130545.48040408806,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563468.4110742}}
+1573563478.4872503
+{'af4eb1': {'temp_mean': 29.528000000000002, 'pressure_mean': 989.05, 'humidity_mean': 32.8244,
+            'tvoc_mean': 130267.06143650867, 'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563478.4839232}}
+1573563488.560256
+{'af4eb1': {'temp_mean': 29.489, 'pressure_mean': 989.049, 'humidity_mean': 32.8853, 'tvoc_mean': 130508.93283618565,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563488.5567725}}
+1573563498.6333315
+{'af4eb1': {'temp_mean': 29.437, 'pressure_mean': 989.038, 'humidity_mean': 32.8395, 'tvoc_mean': 131069.69943322535,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563498.629763}}
+1573563508.7064035
+{'af4eb1': {'temp_mean': 29.483, 'pressure_mean': 989.039, 'humidity_mean': 32.7401, 'tvoc_mean': 131526.96872840222,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563508.7027593}}
+1573563518.783726
+{'af4eb1': {'temp_mean': 29.602, 'pressure_mean': 989.038, 'humidity_mean': 32.5001, 'tvoc_mean': 131633.7285363866,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563518.7801716}}
+1573563528.8570497
+{'af4eb1': {'temp_mean': 29.594, 'pressure_mean': 989.0219999999999, 'humidity_mean': 32.4248, 'tvoc_mean': 131791.50594557475,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563528.8534815}}
+1573563538.9303632
+{'af4eb1': {'temp_mean': 29.785, 'pressure_mean': 989.055, 'humidity_mean': 32.2589, 'tvoc_mean': 131852.03633022658,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563538.9265459}}
+1573563549.003481
+{'af4eb1': {'temp_mean': 29.771, 'pressure_mean': 989.053, 'humidity_mean': 32.1285, 'tvoc_mean': 132106.91389329854,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563548.9997604}}
+1573563559.0765269
+{'af4eb1': {'temp_mean': 29.746, 'pressure_mean': 989.06, 'humidity_mean': 32.2783, 'tvoc_mean': 131889.21330643177,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563559.073168}}
+1573563569.1491516
+{'af4eb1': {'temp_mean': 29.885, 'pressure_mean': 989.062, 'humidity_mean': 32.1445, 'tvoc_mean': 131380.1229771212,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563569.1453593}}
+1573563579.2225404
+{'af4eb1': {'temp_mean': 29.941, 'pressure_mean': 989.061, 'humidity_mean': 32.0107, 'tvoc_mean': 131890.4007419757,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563579.2188833}}
+1573563589.2952425
+{'af4eb1': {'temp_mean': 29.916, 'pressure_mean': 989.049, 'humidity_mean': 31.9236, 'tvoc_mean': 132322.9927292891,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563589.2920458}}
+1573563599.3679304
+{'af4eb1': {'temp_mean': 29.98, 'pressure_mean': 989.077, 'humidity_mean': 31.878800000000002, 'tvoc_mean': 131909.35404592336,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563599.3645601}}
+1573563609.4414544
+{'af4eb1': {'temp_mean': 29.902, 'pressure_mean': 989.071, 'humidity_mean': 31.9007, 'tvoc_mean': 132135.4269111337,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563609.4377866}}
+1573563619.514327
+{'af4eb1': {'temp_mean': 29.788, 'pressure_mean': 989.087, 'humidity_mean': 32.0813, 'tvoc_mean': 132037.26079825725,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563619.5108688}}
+1573563629.5873196
+{'af4eb1': {'temp_mean': 29.77, 'pressure_mean': 989.0840000000001, 'humidity_mean': 32.1527, 'tvoc_mean': 132333.69631262042,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563629.5840738}}
+1573563639.6595733
+{'af4eb1': {'temp_mean': 29.843, 'pressure_mean': 989.081, 'humidity_mean': 32.1078, 'tvoc_mean': 132017.01463739428,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563639.6562476}}
+1573563649.7328527
+{'af4eb1': {'temp_mean': 29.899, 'pressure_mean': 989.05, 'humidity_mean': 32.0254, 'tvoc_mean': 132185.09994642632,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563649.7292185}}
+1573563659.8062773
+{'af4eb1': {'temp_mean': 29.913, 'pressure_mean': 989.031, 'humidity_mean': 32.0009, 'tvoc_mean': 131859.8463247115,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563659.8025894}}
+1573563669.8794603
+{'af4eb1': {'temp_mean': 29.901, 'pressure_mean': 989.026, 'humidity_mean': 32.0119, 'tvoc_mean': 131967.86977776102,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563669.8759813}}
+1573563679.9524422
+{'af4eb1': {'temp_mean': 29.831, 'pressure_mean': 989.053, 'humidity_mean': 32.1074, 'tvoc_mean': 131928.48422693345,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563679.9490206}}
+1573563690.0257766
+{'af4eb1': {'temp_mean': 29.786, 'pressure_mean': 989.087, 'humidity_mean': 32.2879, 'tvoc_mean': 131447.97923998622,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563690.0221334}}
+1573563700.0991492
+{'af4eb1': {'temp_mean': 29.754, 'pressure_mean': 989.048, 'humidity_mean': 32.3614, 'tvoc_mean': 131625.15247842798,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563700.0957434}}
+1573563710.1751635
+{'af4eb1': {'temp_mean': 29.889, 'pressure_mean': 989.0419999999999, 'humidity_mean': 32.2388, 'tvoc_mean': 131605.05915316183,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563710.1713088}}
+1573563720.2482939
+{'af4eb1': {'temp_mean': 30.075, 'pressure_mean': 989.035, 'humidity_mean': 32.0263, 'tvoc_mean': 131762.4942000371,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563720.2447321}}
+1573563730.3213978
+{'af4eb1': {'temp_mean': 30.053, 'pressure_mean': 989.072, 'humidity_mean': 31.856099999999998,
+            'tvoc_mean': 131987.57226771998, 'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563730.3179}}
+1573563740.3949432
+{'af4eb1': {'temp_mean': 30.033, 'pressure_mean': 989.08, 'humidity_mean': 31.8602, 'tvoc_mean': 132147.53110180658,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563740.391255}}
+1573563750.4683952
+{'af4eb1': {'temp_mean': 29.915, 'pressure_mean': 989.06, 'humidity_mean': 31.9859, 'tvoc_mean': 131940.40646624303,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563750.464662}}
+1573563760.541708
+{'af4eb1': {'temp_mean': 29.79, 'pressure_mean': 989.065, 'humidity_mean': 32.2255, 'tvoc_mean': 131644.6235845297,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563760.5378814}}
+1573563770.615227
+{'af4eb1': {'temp_mean': 29.707, 'pressure_mean': 989.0740000000001, 'humidity_mean': 32.4388,
+            'tvoc_mean': 131752.82564621608, 'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563770.6116}}
+1573563780.6859636
+{'af4eb1': {'temp_mean': 29.668, 'pressure_mean': 989.063, 'humidity_mean': 32.5395, 'tvoc_mean': 131516.41435524554,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563780.682393}}
+1573563790.7582855
+{'af4eb1': {'temp_mean': 29.578, 'pressure_mean': 989.0609999999999, 'humidity_mean': 32.8958, 'tvoc_mean': 130637.70187082252,
+            'gas_baseline': 129939.33067442996, 'baseline_temp': 29.19, 'timestamp': 1573563790.7565155}}
