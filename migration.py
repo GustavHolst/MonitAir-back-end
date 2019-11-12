5250557aa5e051f064730bc87b26e882d46bd99d
WJcaine <billy.caine@gmail.com>
2019-11-12 11:34:34 +0000

    managed to get a single seed working

--- migration.py
+++ migration.py
@@ -2,7 +2,7 @@ import psycopg2
 from config import config
 
 def create_tables():
-    commands = (
+    commands = [(
         """
         CREATE TABLE data (
             reading_id SERIAL PRIMARY KEY,
@@ -10,10 +10,32 @@ def create_tables():
             pressure FLOAT,
             humidity FLOAT,
             TVOC FLOAT,
-            gasBaseline Float
-        )
+            gasBaseline FLOAT,
+            user_id int
+        );
         """
-    )
+    )]
     conn = None
     try:
-        params=config()
+        # read the connection parameters
+        params = config()
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(dbname= params['database'],user = params['user'],password = params['password'])
+        cur = conn.cursor()
+        # create table one by one
+        for command in commands:
+            print(command)
+            cur.execute(command)
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+ 
+ 
+if __name__ == '__main__':
+    create_tables()
