6b0c0c28ed4b97ab7d0d878b67a0252e85dcaf1a
Eamon Sharifi <eamon_s@hotmail.co.uk>
2019-11-14 18:38:17 +0000

    refactor response for get_readings to include key-value pair

--- api/app.py
+++ api/app.py
@@ -9,9 +9,10 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Database initialisation
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-    os.path.join(basedir, 'db.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+    basedir, "db.sqlite"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
@@ -33,6 +34,7 @@ class User(db.Model):
         self.sensor_id = sensor_id
         self.username = username
 
+
 # Reading Class / Model
 
 
@@ -42,12 +44,21 @@ class Reading(db.Model):
     pressure_mean = db.Column(db.Float)
     humidity_mean = db.Column(db.Float)
     tvoc_mean = db.Column(db.Float)
-    sensor_id = db.Column(db.String(20), db.ForeignKey('user.sensor_id'))
+    sensor_id = db.Column(db.String(20), db.ForeignKey("user.sensor_id"))
     timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     baseline_temp = db.Column(db.Float)
     gas_baseline = db.Column(db.Float)
 
-    def __init__(self, temp_mean, pressure_mean, humidity_mean, tvoc_mean, sensor_id, baseline_temp, gas_baseline):
+    def __init__(
+        self,
+        temp_mean,
+        pressure_mean,
+        humidity_mean,
+        tvoc_mean,
+        sensor_id,
+        baseline_temp,
+        gas_baseline,
+    ):
         self.temp_mean = temp_mean
         self.pressure_mean = pressure_mean
         self.humidity_mean = humidity_mean
@@ -56,37 +67,47 @@ class Reading(db.Model):
         self.baseline_temp = baseline_temp
         self.gas_baseline = gas_baseline
 
+
 # User Schema (using marshmallow)
 
 
 class UserSchema(ma.Schema):
     class Meta:
-        fields = ('user_id', 'first_name', 'surname',
-                  'email', 'sensor_id', 'username')
+        fields = ("user_id", "first_name", "surname", "email", "sensor_id", "username")
+
 
 # Reading Schema
 
 
 class ReadingSchema(ma.Schema):
     class Meta:
-        fields = ('reading_id', 'temp_mean', 'pressure_mean', 'humidity',
-                  'tvoc_mean', 'sensor_id', 'timestamp', 'baseline_temp', 'gas_baseline')
+        fields = (
+            "reading_id",
+            "temp_mean",
+            "pressure_mean",
+            "humidity_mean",
+            "tvoc_mean",
+            "sensor_id",
+            "timestamp",
+            "baseline_temp",
+            "gas_baseline",
+        )
 
 
 # Init Schema
 user_schema = UserSchema()  # May need to add strict=True to both invokations
-users_schema = UserSchema(many=True)
+# users_schema = UserSchema(many=True)
 reading_schema = ReadingSchema()
 readings_schema = ReadingSchema(many=True)
 
 
-@app.route('/user', methods=['POST'])
+@app.route("/user", methods=["POST"])
 def post_user():
-    first_name = request.json['first_name']
-    surname = request.json['surname']
-    email = request.json['email']
-    sensor_id = request.json['sensor_id']
-    username = request.json['username']
+    first_name = request.json["first_name"]
+    surname = request.json["surname"]
+    email = request.json["email"]
+    sensor_id = request.json["sensor_id"]
+    username = request.json["username"]
 
     new_user = User(first_name, surname, email, sensor_id, username)
 
@@ -96,45 +117,54 @@ def post_user():
     return user_schema.jsonify(new_user)
 
 
-@app.route('/user/<username>', methods=['GET'])
+@app.route("/user/<username>", methods=["GET"])
 def get_user(username):
     user = User.query.filter_by(username=username).first()
-    print(user)
     return user_schema.jsonify(user)
 
 
-@app.route('/reading/<sensor_id>', methods=['POST'])
+@app.route("/reading/<sensor_id>", methods=["POST"])
 def post_reading(sensor_id):
     sensor_id = sensor_id
-    temp_mean = request.json[sensor_id]['temp_mean']
-    pressure_mean = request.json[sensor_id]['pressure_mean']
-    humidity_mean = request.json[sensor_id]['humidity_mean']
-    tvoc_mean = request.json[sensor_id]['tvoc_mean']
-    gas_baseline = request.json[sensor_id]['gas_baseline']
-    baseline_temp = request.json[sensor_id]['baseline_temp']
+    temp_mean = request.json[sensor_id]["temp_mean"]
+    pressure_mean = request.json[sensor_id]["pressure_mean"]
+    humidity_mean = request.json[sensor_id]["humidity_mean"]
+    tvoc_mean = request.json[sensor_id]["tvoc_mean"]
+    gas_baseline = request.json[sensor_id]["gas_baseline"]
+    baseline_temp = request.json[sensor_id]["baseline_temp"]
 
-    new_reading = Reading(temp_mean, pressure_mean, humidity_mean,
-                          tvoc_mean, sensor_id, baseline_temp, gas_baseline)
+    new_reading = Reading(
+        temp_mean,
+        pressure_mean,
+        humidity_mean,
+        tvoc_mean,
+        sensor_id,
+        baseline_temp,
+        gas_baseline,
+    )
 
     db.session.add(new_reading)
     db.session.commit()
-    return reading_schema.jsonify(new_reading)
+    print(reading_schema)
+    # return reading_schema.jsonify({"reading": new_reading})
+    return reading_schema.jsonify(new_reading), 201
 
 
-@app.route('/reading/<sensor_id>', methods=['GET'])
+@app.route("/reading/<sensor_id>", methods=["GET"])
 def get_readings(sensor_id):
     all_readings_for_sensor = Reading.query.filter_by(sensor_id=sensor_id)
     result = readings_schema.dump(all_readings_for_sensor)
-    return jsonify(result)
+    print(result)
+    return jsonify({"readings": result})
+
+
+@app.route("/most_recent_reading/<sensor_id>", methods=["GET"])
+def get_most_recent_reading(sensor_id):
+    most_recent_reading = Reading.query.filter_by(sensor_id=sensor_id)
+    print(most_recent_reading)
+    return reading_schema.jsonify(most_recent_reading)
 
 
 # Run server
 if __name__ == "__main__":
     app.run(debug=True)
-
-# def setUp(self):
-#   app.db.session.commit()
-#   app.db.drop_all()
-#   app.db.create_all()
-
-# setUp(app)
